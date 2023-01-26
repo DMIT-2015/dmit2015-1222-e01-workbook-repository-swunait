dc50f9c700ab63bd5b8b6539b1812b5ff9effc52
Sam Wu <swu@nait.ca>
2023-01-25 20:47:27 -0700

    Added CSV file

---
+++ dmit2015-azure-webapp/src/main/java/dmit2015/model/CanadianPersonalIncomeTaxManager.java
@@ -0,0 +1,27 @@
+package dmit2015.model;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class CanadianPersonalIncomeTaxManager {
+
+    public static void main(String[] args) {
+        try {
+            Path csvPath = Path.of(Thread.currentThread().getContextClassLoader().getResource("data/CanadianPersonalIncomeTaxRates.csv").toURI());
+            // Use the `Files.readAllLines()` method to read all lines from a file as a List.
+            System.out.println("I am reading all the lines from the csv file into a list of String.");
+            List allLines = Files.readAllLines(csvPath);
+            System.out.println("I am printing each line read from the list.");
+            allLines.forEach(System.out::println);
+            System.out.println("\n\n");
+
+            
+        } catch (URISyntaxException | IOException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+}
