79760d2762230a8a378907351b59c511c4864517
Sam Wu <swu@nait.ca>
2023-02-03 20:57:43 -0700

    Added rectangle.xhtml

--- dmit2015-faces-demo/src/main/java/dmit2015/faces/CircleRequestScopedController.java
+++ dmit2015-faces-demo/src/main/java/dmit2015/faces/CircleRequestScopedController.java
@@ -8,29 +8,30 @@ import org.omnifaces.util.Messages;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
-import java.io.Serializable;
-
 @Named("currentCircleRequestScopedController")
 @RequestScoped  // create this object for one HTTP request and destroy after the HTTP response has been sent
 public class CircleRequestScopedController {
 
     @Getter
     @Setter
-    private Circle currentCircle = new Circle();
+    private double radius;
 
     public String showArea() {
+        var currentCircle = new Circle(radius);
         Messages.addGlobalInfo("The area of a circle with a {0} radius is {1}",
                 currentCircle.getRadius(), currentCircle.area());
         return null;
     }
 
     public String showDiameter() {
+        var currentCircle = new Circle(radius);
         Messages.addGlobalInfo("The diameter of a circle with a {0} radius is {1}",
                 currentCircle.getRadius(), currentCircle.diameter());
         return null;
     }
 
     public String showCircumference() {
+        var currentCircle = new Circle(radius);
         Messages.addGlobalInfo("The circumference of a circle with a {0} radius is {1}",
                 currentCircle.getRadius(), currentCircle.circumference());
         return null;
